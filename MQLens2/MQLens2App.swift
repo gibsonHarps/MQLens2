fdcc8def0b5de0a73f127fd02da8bf100fd7a9aa
Hackathon <sophia.karski@db.com>
2022-09-08 20:11:46 +0100

    Initial Commit

---
+++ MQLens2/MQLens2App.swift
@@ -0,0 +1,17 @@
+//
+//  MQLens2App.swift
+//  MQLens2
+//
+//  Created by hackathon on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MQLens2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
