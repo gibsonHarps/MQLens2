b4ed3bfdf27308ef65f8025b0a53e1e25f25b999
Hackathon <sophia.karski@db.com>
2022-09-08 20:17:26 +0100

    a different attempt on mobile ui

--- MQLens2/ContentView.swift
+++ MQLens2/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        NavigationView {
+            List {
+                Section {
+                    Text("About")
+                    Text("Software Update")
+                }
+                Section {
+                    Text("AirDrop")
+                    Text("AirPlay & Handoff")
+                    Text("Picture in Picture")
+                    Text("CarPlay")
+                }
+                Section {
+                    Text("iPhone Storage")
+                    Text("Background App Refresh")
+                }
+                Section {
+                    Text("Date & Time")
+                    Text("Keyboard")
+                    Text("Fonts")
+                    Text("Language & Region")
+                }
+            }
+            .navigationTitle("General")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
